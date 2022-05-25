43f242cedf4365bbdab7e0e9925c5ba5126636e6
Daniel Matthee <daniel.matthee@qrious.co.nz>
2022-05-26 00:41:58 +1200

    Add files via upload

--- macros/ds_and_dim_initial_create_sql.sql
+++ macros/ds_and_dim_initial_create_sql.sql
@@ -1,29 +1,32 @@
 {%- macro ds_and_dim_initial_create_sql(target_relation, temp_relation, change_key) -%}
-{% set distant_past  ="to_timestamp_ltz('1900-01-01 00:00:00.000')"%}
-{% set distant_future ="to_timestamp_ltz('2999-12-31 23:59:59.000')"%}
-{{ create_table_as(True, temp_relation, sql) }}
-{% set CurrentTimeStamp ="to_timestamp_ltz(current_timestamp())"%}
-{%- set initial_sql -%}
-{% if change_key != 'unknown'  %}
--- Type 2 solution
-SELECT
-*
-,{{ distant_past }} as DSS_START_DATE
-,{{ distant_future }} as DSS_END_DATE
-, 'Y' as DSS_CURRENT_FLAG
-,{{ CurrentTimeStamp }} as DSS_CREATE_DATE
-,{{ CurrentTimeStamp }} as DSS_UPDATE_DATE
-FROM
-{{ temp_relation }}
-{%else%}
--- Type 1 solution
-SELECT
-*
-,{{ CurrentTimeStamp }} as DSS_CREATE_DATE
-,{{ CurrentTimeStamp }} as DSS_UPDATE_DATE
-FROM
-{{ temp_relation }}
-{%endif%}
-{%- endset -%}
-{{ create_table_as(False, target_relation, initial_sql) }}
+    {% set distant_past  ="to_timestamp_ltz('1900-01-01 00:00:00.000')"%}
+    {% set distant_future ="to_timestamp_ltz('2999-12-31 23:59:59.000')"%}
+    {{ create_table_as(True, temp_relation, sql) }}
+    {% set CurrentTimeStamp ="to_timestamp_ltz(current_timestamp())"%}
+  
+    {%- set initial_sql -%}
+    {% if change_key != 'unknown'  %}
+     -- Type 2 solution
+      SELECT
+        *
+        ,{{ distant_past }} as DSS_START_DATE
+        ,{{ distant_future }} as DSS_END_DATE
+        , 'Y' as DSS_CURRENT_FLAG
+        ,{{ CurrentTimeStamp }} as DSS_CREATE_DATE
+        ,{{ CurrentTimeStamp }} as DSS_UPDATE_DATE
+      FROM
+        {{ temp_relation }}
+   
+    {%else%}
+    -- Type 1 solution
+      SELECT
+        *
+        ,{{ CurrentTimeStamp }} as DSS_CREATE_DATE
+        ,{{ CurrentTimeStamp }} as DSS_UPDATE_DATE
+      FROM
+        {{ temp_relation }}
+    {%endif%}
+    {%- endset -%}
+    {{ create_table_as(False, target_relation, initial_sql) }}
 {%- endmacro -%}
+ 
--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -1,22 +1,23 @@
 {{ config(materialized='ds_and_dim_creator',
-business_key='businessentityid'
-)
-}}
-SELECT SEQ_DIM_KEYS.nextval as dim_customer_key , stage_customer.businessentityid  businessentityid
-, stage_customer.title  title
-, stage_customer.firstname  firstname
-, stage_customer.middlename  middlename
-, stage_customer.lastname  lastname
-, stage_customer.suffix  suffix
-, stage_customer.phonenumber  phonenumber
-, stage_customer.phonenumbertype  phonenumbertype
-, stage_customer.emailaddress  emailaddress
-, stage_customer.emailpromotion  emailpromotion
-, stage_customer.addresstype  addresstype
-, stage_customer.addressline1  addressline1
-, stage_customer.addressline2  addressline2
-, stage_customer.city  city
-, stage_customer.stateprovincename  stateprovincename
-, stage_customer.postalcode  postalcode
-, stage_customer.countryregionname  countryregionname
-FROM {{ ref('stage_customer')}} stage_customer
+          business_key='businessentityid' 
+        )
+}} 
+  SELECT SEQ_DIM_KEYS.nextval as dim_customer_key , stage_customer.businessentityid  businessentityid
+         , stage_customer.title  title
+         , stage_customer.firstname  firstname
+         , stage_customer.middlename  middlename
+         , stage_customer.lastname  lastname
+         , stage_customer.suffix  suffix
+         , stage_customer.phonenumber  phonenumber
+         , stage_customer.phonenumbertype  phonenumbertype
+         , stage_customer.emailaddress  emailaddress
+         , stage_customer.emailpromotion  emailpromotion
+         , stage_customer.addresstype  addresstype
+         , stage_customer.addressline1  addressline1
+         , stage_customer.addressline2  addressline2
+         , stage_customer.city  city
+         , stage_customer.stateprovincename  stateprovincename
+         , stage_customer.postalcode  postalcode
+         , stage_customer.countryregionname  countryregionname
+    FROM {{ ref('stage_customer')}} stage_customer
+    
