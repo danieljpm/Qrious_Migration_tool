c9bded5063543f2996c69fcdbdc27687344a4ec0
Daniel Matthee <daniel.matthee@qrious.co.nz>
2022-05-25 16:11:41 +1200

    Add files via upload

--- macros/ds_and_dim_creator.sql
+++ macros/ds_and_dim_creator.sql
@@ -2,7 +2,7 @@
 {%- set business_key = config.get('business_key') -%}
 {%- set change_key = config.get('change_key',default='unknown') -%}
 {%- set target_relation = this %}
-{%- set tmp_identifier = "temp_" \ target_relation.identifier %}
+{%- set tmp_identifier = "temp_" ~ target_relation.identifier %}
 {%- set tmp_relation = target_relation.incorporate(path= {"identifier": tmp_identifier, "schema": config.get('temp_schema', default=target_relation.schema)}) -%}
 {%- set existing_relation = load_relation(this) -%}
 {% if existing_relation is none or should_full_refresh() %}
@@ -19,5 +19,5 @@
 {{ run_hooks(post_hooks) }}
 {% set target_relation = this.incorporate(type='table') %}
 {% do persist_docs(target_relation, model) %}
-{{ return({'relations': \target_relation\}) }}
+{{ return({'relations': [target_relation]}) }}
 {% endmaterialization %}
